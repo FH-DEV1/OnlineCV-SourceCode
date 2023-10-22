fba855c7cbbd3901b51ecf170d58d05471a72906
FH.Dev <felixpoletti5@gmail.com>
2023-10-22 12:21:30 +0200

    added home animation

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 
 const Home: React.FC = () => {
+    const spanClass = "pl-1 animate-spin-words text-blue-500 text-2xl"
     return (
         <div className="flex flex-1 flex-grow items-center justify-center flex-col">
             <Link href="mailto:FHDEV@proton.me?subject=Job offer" className="cursor-pointer absolute left-[8vw] bottom-[8vw] md:left-[3vw] md:bottom-[3vw] backdrop-blur-md dark:bg-opacity-20 bg-opacity-20 bg-gray-300 dark:bg-gray-400 w-20 h-8 flex justify-center items-center border border-emerald-500 rounded-md">
@@ -12,7 +13,22 @@ const Home: React.FC = () => {
                 </span>
                 <h1 className="text-emerald-500">Hire Me</h1>
             </Link>
-            <h1>Home</h1>
+            <div className="flex flex-row h-9 overflow-hidden hover:overflow-visible">
+                <h1 className="text-2xl">I'm comfortable with </h1>
+                <div className="flex flex-col">
+                    <span className={spanClass}>React</span>
+                    <span className={spanClass}>Expo</span>
+                    <span className={spanClass}>Typescript</span>
+                    <span className={spanClass}>Next.js</span>
+                    <span className={spanClass}>Tailwind</span>
+                    <span className={spanClass}>npm/yarn</span>
+                    <span className={spanClass}>HTML5</span>
+                    <span className={spanClass}>CSS</span>
+                    <span className={spanClass}>Javascript</span>
+                    <span className={spanClass}>React</span>
+                </div>
+            </div>
+            
         </div>
     )
 };
